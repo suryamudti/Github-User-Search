76c0116eea97c8bf9b293abbfdce0281bc16977f
suryamudti <suryamudti0128@gmail.com>
2019-09-21 17:15:47 +0700

    update viewmodel

--- app/src/main/java/com/surya/githubusersearchapp/ui/MainViewModel.kt
+++ app/src/main/java/com/surya/githubusersearchapp/ui/MainViewModel.kt
@@ -1,7 +1,12 @@
 package com.surya.githubusersearchapp.ui
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
+import androidx.paging.PagedList
+import com.surya.githubusersearchapp.data.model.GitUser
+import com.surya.githubusersearchapp.data.model.UserSearchResult
 import com.surya.githubusersearchapp.data.repositories.GithubRepository
 
 /**
@@ -12,4 +17,30 @@ class MainViewModel(
 ) : ViewModel() {
 
     private val queryLiveData = MutableLiveData<String>()
+
+    private val userResult: LiveData<UserSearchResult> = Transformations.map(queryLiveData) {
+        repository.search(it) // returns UserSearchResult live data by executing search method from GithubRepository with every update to the queryLiveData
+    }
+
+
+    val users: LiveData<PagedList<GitUser>> = Transformations.switchMap(userResult) { it.data } // returns the list of users live data from UserSearchResult
+    val networkErrors: LiveData<String> = Transformations.switchMap(userResult) {
+        it.networkErrors // // returns error string live data from UserSearchResult
+    }
+
+    /**
+     * Search a user based on a query string.
+     * post a new value from queryLiveData
+     * resulting in initiating a search request from repository manager through userResult
+     */
+    fun searchUser(queryString: String) {
+        queryLiveData.postValue(queryString)
+    }
+
+
+
+    /**
+     * Get the last query value.
+     */
+    fun lastQueryValue(): String? = queryLiveData.value
 }
